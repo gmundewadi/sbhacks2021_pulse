e08fe70c2a9db412e4499006f9d3f09ebd5e4cf8
Gautam Mundewadi <gautammundewadi@gmail.com>
2021-01-16 17:04:16 -0800

    gm - removed extra outline from slider

--- components/student_components/Slider.js
+++ components/student_components/Slider.js
@@ -12,8 +12,8 @@ export default function Slider(props) {
       onChange={val => props.setPulse(val)}
       orientation="vertical"
       invert
-      pearling
-      minDistance={10}
+      // pearling
+      // minDistance={10}
     />
   );
 }
