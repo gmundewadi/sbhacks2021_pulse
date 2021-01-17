945971b1129ab174615c4dfeec8586eca3f05da4
Tyler Ekaireb <tylerekaireb@gmail.com>
2021-01-17 09:00:56 -0800

    fixed student view

--- components/student_components/StudentMenu.js
+++ components/student_components/StudentMenu.js
@@ -4,13 +4,13 @@ import '../../styles/StudentMenu.css';
 export default function StudentMenu(props) {
 
     return (
-        <div className={'menu'}>
-            <div className={'code'}>Code: {props.roomID}</div>
+        <div className={'student_menu'}>
+            <div className={'student_code'}>Code: {props.roomID}</div>
                 {props.menu_items.map((item, index) =>
                     <span key={item} onClick={e => props.goToPage(item)}
                         className={props.active_page === item ? 'active' : ''}>{item}</span>
                 )}
-            <div className={'byline'}>Made by Tyler & Gautam<br />SBHacks 2021</div>
+            {/* <div className={'byline'}>Made by Tyler & Gautam<br />SBHacks 2021</div> */}
         </div>
     );
 }
